7f3a92cd9404e1509c9cbf44ab9bdbc7e0a032f7
mhmxd <mahmoud.sadeghi@aau.at>
2026-02-09 08:22:39 +0100

    [+] GestureLog

---
+++ Common/Logs/GestureLog.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Logs
+{
+    public class GestureLog
+    {
+        public long timestamp;           // timestamp of the gesture (ms since epoch)
+        public string finger = "";       // finger performing the gesture
+        public string action = "";       // action type (e.g., tap, swipe)
+        public string x = "";            // x coordinate of the gesture (if applicable)
+        public string y = "";            // y coordinate of the gesture (if applicable)
+    }
+}
